ecbd55aaa1068e68a2c805aae1aa24ea60d41507
hatrongvu <vuht7@msb.com.vn>
2025-03-14 00:27:30 +0700

    update code setup worker node

--- k3s/setup.sh
+++ k3s/setup.sh
@@ -2,5 +2,5 @@
 curl -sfL https://get.k3s.io K3S_KUBECONFIG_MODE="644" sh -s
 
 #install worker node
-curl -sfL https://get.k3s.io K3S_TOKEN="YOUR_TOKEN" K3S_URL"https://[your_server_id]:6443" K3S_NODE_NAME="servername" sh -
+curl -sfL https://get.k3s.io | K3S_TOKEN="YOUR_TOKEN" K3S_URL"https://[your_server_id]:6443" K3S_NODE_NAME="servername" sh -
 
